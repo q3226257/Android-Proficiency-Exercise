6b057c58e7efcba87cc520a1375677ec26062af2
q3226257 <616784622@qq.com>
2017-07-25 21:34:20 +0800

    loss file

--- myapplication/src/main/java/com/example/myapplication/View/view_i/IBaseA2F.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.View.view_i;
-
-
-/**
- * 定义从 fragment 向 activity 发送指令的接口
- */
-public interface IBaseA2F<T> {
-    void refreshView(T modifiedData,T modifyData);
-}
