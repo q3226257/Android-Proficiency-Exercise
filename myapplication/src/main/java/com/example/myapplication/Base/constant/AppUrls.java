f606acdfaaa1ddce13faa30e24db8f26fcc0cc00
q3226257 <616784622@qq.com>
2017-07-25 21:32:36 +0800

    loss file

---
+++ myapplication/src/main/java/com/example/myapplication/Base/constant/AppUrls.java
@@ -0,0 +1,9 @@
+package com.example.myapplication.Base.constant;
+
+/**
+ * Created by cfadmin on 2017/7/18.
+ */
+public class AppUrls {
+    public static final String BASE_URL = "http://gank.io/api/";
+
+}
